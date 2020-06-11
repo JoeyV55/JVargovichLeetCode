cf7eebf209dbd854601679f3dca2bfa73c9203c4
Joseph Vargovich <46722295+DovahCraft@users.noreply.github.com>
2020-06-10 22:06:04 -0700

    Created Valid Parenthesis java file.

---
+++ ValidParenthesis.java
@@ -0,0 +1,43 @@
+//Link to problem: https://leetcode.com/problems/valid-parentheses/
+
+import java.util.*;
+
+class Solution {
+        public boolean isValid(String s) {
+        Stack<Character> currentChars = new Stack<>();
+        if(s == null || s.isEmpty())    
+            return true;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(isLeftChar(s.charAt(i)))
+                currentChars.push(s.charAt(i));
+            else if(!isMatchingChar(currentChars, s.charAt(i)))
+               return false;                 
+            
+            
+        }
+            
+        return currentChars.empty();
+    }
+    
+    
+    public boolean isLeftChar(char currentChar){
+        return currentChar == '(' || currentChar == '[' || currentChar == '{';  
+    }
+    
+                    
+    public boolean isMatchingChar(Stack<Character> charStack, char currentChar){
+        if(!charStack.isEmpty()){
+            if(currentChar == ')')
+                return charStack.pop() == '(';
+            
+            if(currentChar == '}')
+                return charStack.pop() == '{';
+                
+            if(currentChar == ']')
+                return charStack.pop() == '[';
+        }
+        return false;
+    }
+    
+}
